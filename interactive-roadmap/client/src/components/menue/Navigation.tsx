9ad746be0e1f938589df4b7511f5c14ffe28f3f0
Julian Zhou <junlan.zx@alibaba-inc.com>
2021-09-15 00:02:30 +0800

    Update interactive-roadmap

--- interactive-roadmap/client/src/components/menue/Navigation.tsx
+++ interactive-roadmap/client/src/components/menue/Navigation.tsx
@@ -170,8 +170,8 @@ function Navigation({
           </div>
         )}
         <div className='menue-bar__links'>
-          {!isAuthenticated && (
-            /**<>
+          {!isAuthenticated /**  && (
+            <>
               <Link to='/login' className='menue-bar__link'>
                 Login
               </Link>
@@ -179,8 +179,8 @@ function Navigation({
                 Register
                 <span className='menue-bar__free-indicator'>(free)</span>
               </Link>
-            </>**/
-          )}
+            </>
+          ) **/}
           {(currentPreset.length || isAuthenticated) && (
             <button className='menue-bar__link' onClick={handleSave}>
               Save
